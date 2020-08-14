a00fe6674d2a562cbaf300258e199d003c8322af
Maxim Kostick <maximkte73@gmail.com>
2020-08-14 18:18:14 +0300

    add 01

--- JS_HW_5/01.ts
+++ JS_HW_5/01.ts
@@ -2,11 +2,16 @@
 // - Функция для вывода на экран информации об автомобиле
 // - Функция для подсчета необходимого времени для преодоления переданного расстояния со средней скоростью. Учтите, что через каждые 4 часа дороги водителю необходимо делать перерыв на 1 час
 
+let averageSpeed =  {
+   time: 1,
+   distance: 100
+}
+
 let car = {
    manufacturer: 'BMW',
    model: 'x6',
    issueYear: 2020,
-   averageSpeed: '120км/час'
+   averageSpeed: `${averageSpeed.distance}км/${averageSpeed.time}час`
 }
 
 function carInfo(car) {
@@ -18,4 +23,16 @@ function carInfo(car) {
 }
 
 carInfo(car);
+
+function calcTime(userDistance: number) {
+   let count = 0;
+   let needTime = userDistance / averageSpeed.distance;
+   for (let i = 0; i <= needTime; i += 4) {
+      count++
+   }
+   alert((userDistance / averageSpeed.distance) + count - 1);
+   
+}
+
+calcTime(+prompt('Введите расстояние которое нужно проехать'));
 
