38b75a274c53da6f388bb3324c7760bc475cac26
Maxim Kostick <46405376+MaximKostick@users.noreply.github.com>
2020-08-14 08:50:38 +0300

    Delete 01.js

--- JS_HW_3/01.js
+++
@@ -1,9 +0,0 @@
-// Подсчитать сумму всех чисел в заданном пользователем диапазоне
-var minNumber = +prompt('Введите минимальное число');
-var maxNumber = +prompt('Введите максимальное число');
-var sum = 0;
-while (minNumber <= maxNumber) {
-    sum += minNumber;
-    minNumber++;
-}
-alert(sum);
