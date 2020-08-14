10fdb646f1cd53edbba18107aaabfc0e248c05dd
Maxim Kostick <46405376+MaximKostick@users.noreply.github.com>
2020-08-14 08:53:15 +0300

    Delete 03.js

--- JS_HW_4/03.js
+++
@@ -1,5 +0,0 @@
-// Написать функцию, которая принимает три отдельные цифры и превращает их в одно число. Например: цифры 1, 4, 9 превратятся в число 149
-function printNumber(number1, number2, number3) {
-    alert("" + number1 + number2 + number3);
-}
-printNumber(+prompt('Число 1'), +prompt('Число 2'), +prompt('Число 3'));
