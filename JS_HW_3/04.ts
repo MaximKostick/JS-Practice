77743ad90ef0a6df717e5dd8a3c42134eaef0f51
Maxim Kostick <maximkte73@gmail.com>
2020-08-14 08:54:59 +0300

    add 04

--- JS_HW_3/04.ts
+++ JS_HW_3/04.ts
@@ -1,1 +1,20 @@
-// 4 Определить количество цифр в введенном числе.
+// 4 Определить количество цифр в введенном числе
+
+function numberQuantity(userNumber) {
+   if (userNumber < 0) {
+      userNumber *= -1;
+   }
+   let count: number = 0;
+   for (let i = 10; userNumber > 1; userNumber /= i) {
+      count++
+   }
+   if (userNumber == 1) {
+      alert('1'); 
+   }
+   else {
+      alert(count);
+   }
+
+}
+
+numberQuantity(+prompt('Введите число'));
