fb61a5c34f9996aea2f643c88b2b8c093a8ea8da
Maxim Kostick <maximkte73@gmail.com>
2020-08-18 14:22:33 +0300

    add 02

---
+++ JS_HW_5/02.ts
@@ -0,0 +1,71 @@
+// Создать объект, хранящий в себе отдельно числитель и знаменатель дроби, и следующие функции для работы с этим объектом. 
+// - Функция сложения 2-х объектов-дробей.
+// - Функция вычитания 2-х объектов-дробей.
+// - Функция умножения 2-х объектов-дробей.
+// - Функция деления 2-х объектов-дробей.
+// - Функция сокращения объекта-дроби.
+
+  interface fraction {
+   upNumber:number,
+   downNumber:number
+}
+
+// Умножение
+
+function fractionMultiplication (firstFr:fraction, secondFr:fraction){
+   let upNumbersMultiply = firstFr.upNumber * secondFr.upNumber;
+   let downNumbersMultiply = firstFr.downNumber * secondFr.downNumber;
+   alert(`${upNumbersMultiply}/${downNumbersMultiply}`);
+}
+
+// Деление
+
+function fractionDivision (firstFr:fraction, secondFr:fraction){
+   let upNumbersMultiply = firstFr.upNumber * secondFr.downNumber;
+   let downNumbersMultiply = firstFr.downNumber * secondFr.upNumber;
+   alert(`${upNumbersMultiply}/${downNumbersMultiply}`);
+}
+
+// Сложение
+
+function fractionPlus (firstFr:fraction, secondFr:fraction){
+   let minDownNumber:number;
+if (firstFr.downNumber > secondFr.downNumber) {
+   minDownNumber = secondFr.downNumber
+}
+if (firstFr.downNumber < secondFr.downNumber) {
+   minDownNumber = firstFr.downNumber
+}
+if (firstFr.downNumber == secondFr.downNumber) {
+   let upNumbersAdd = firstFr.upNumber + secondFr.upNumber;
+   alert(`${upNumbersAdd}/${firstFr.downNumber}`)
+}
+let i
+  for ( i = minDownNumber; i % firstFr.downNumber != 0 && i % secondFr.downNumber != 0; i++) {
+     
+  }
+  let upNumbersAdd = firstFr.upNumber + secondFr.upNumber;
+  alert(`${upNumbersAdd}/${i}`)
+}
+
+// Вычитание
+
+function fractionMinus (firstFr:fraction, secondFr:fraction){
+   let minDownNumber:number;
+if (firstFr.downNumber > secondFr.downNumber) {
+   minDownNumber = secondFr.downNumber
+}
+if (firstFr.downNumber < secondFr.downNumber) {
+   minDownNumber = firstFr.downNumber
+}
+if (firstFr.downNumber == secondFr.downNumber) {
+   let upNumbersAdd = firstFr.upNumber - secondFr.upNumber;
+   alert(`${upNumbersAdd}/${firstFr.downNumber}`)
+}
+let i
+  for ( i = minDownNumber; i % firstFr.downNumber != 0 && i % secondFr.downNumber != 0; i++) {
+     
+  }
+  let upNumbersAdd = firstFr.upNumber - secondFr.upNumber;
+  alert(`${upNumbersAdd}/${i}`)
+}
